69b2d48d4be769741643d2a1c3fef06d41253e3b
luka <antonic.luka88@gmail.com>
2017-05-22 20:00:11 +0200

    ClusterBeard for multiple tile sources

--- tilebeard/tilebeard.py
+++ tilebeard/tilebeard.py
@@ -37,23 +37,28 @@ def get_tile_type(path, url, source): # graceful as a drunk bear...
 
 class TileBeard:
     '''
-    The adapter for serving a set of tiles. Is bound to dir and/or url.
+    The adapter for serving a set of tiles.
     '''
 
-    def __init__(self, path='', url='', sourcefile='', template='/{}/{}/{}', frmt='png', max_workers=2, compresslevel=0):
-        # assert not (path is None and url is None and source is None)
+    def __init__(self, path='', url='', sourcefile='', template='/{}/{}/{}', frmt='png', compresslevel=0, max_workers=5, executor=None, session=None):
+        assert not (path is None and url is None and source is None)
         self.path = path
         self.url = url
         self.template = template
-        if frmt:
-            self.template += '.' + frmt
         self.format = frmt
-        self.executor = ThreadPoolExecutor(max_workers=max_workers)
-        self.source = None
+        if executor is None:
+            self.executor = ThreadPoolExecutor(max_workers=max_workers)
+        else:
+            self.executor = executor
         if sourcefile:
             self.source = ImageSource(source, self.executor)
+            self.format = string.split('.')[-1].lower()
+        else:
+            self.source = None
+            self.format = frmt
+        self.template += '.' + self.format
         self.compresslevel = compresslevel
-        self.session = None
+        self.session = session
         self.tile = get_tile_type(path, url, sourcefile)
         if path is not None:
             stars = '*' * template.count('{}')
@@ -67,19 +72,15 @@ class TileBeard:
     async def __call__(self, key, request_headers={}, filter=None):
         if self.session is None and self.url is not None:
             self.session = ClientSession()
-        if type(key) in (tuple, list):
-            async def sourcecall():
-                return await self.source(key)
-            key = self.template.format(*key)
-        elif self.tile is LazyTile:
-            raise ValueError('LazyTile call requires tuple (z, x, y)')
+        path = self.path + self.template.format(*key)
+        url = self.path + self.template.format(*key)
 
         try:
             with self.tile(
-                self.path+key,
+                path,
                 self.executor,
                 self.compresslevel,
-                self.url+key,
+                url,
                 self.session,
                 self.source,
                 key,
@@ -109,9 +110,30 @@ class TileBeard:
 
 class ClusterBeard:
     '''
-    Adapter for serving multiple layers (TileBeards) piled up in a single
-    directory (meant only for on-demand tiles and .mbtiles in the future, since TileBeard can already handle this on its own for premade pyramids and urls)
+    Adapter for serving multiple layers (TileBeards).
+    Meant only for on-demand tiles (and .mbtiles in the future), since
+    TileBeard can already handle this on its own for premade pyramids and
+    proxy urls by passing custom template arguments.
     '''
 
-    def __init__(self):
-        pass
+    def __init__(self, sourcepath, tilepath='', compresslevel=0, max_workers=5, executor=None):
+        self.sourcepath = sourcepath # formattable string to pass arguments to
+        self.tilepath = tilepath
+        self.compresslevel = compresslevel
+        if executor is None:
+            self.executor = ThreadPoolExecutor(max_workers=max_workers)
+        else:
+            self.executor = executor
+
+    async def __call__(self, key, request_headers={}, filter=None):
+        beard = TileBeard(
+            sourcefile = self.sourcepath.format(*key[:-3]),
+            path = self.tilepath,
+            compresslevel = self.compresslevel,
+            executor = self.executor, # joint executor for all childbeards
+        )
+        return await beard(
+            key[-3:],
+            request_headers=request_headers,
+            filter=filter
+        )
